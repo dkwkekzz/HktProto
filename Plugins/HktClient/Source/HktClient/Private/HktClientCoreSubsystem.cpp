5090b4da3ab0b640839b40ec7eee25ea3785bc5a
YTK <dkwkekzz@naver.com>
2025-10-01 00:05:51 +0900

    패킷 구조 변경: HktClientCoreSubsystem 수정

--- Plugins/HktClient/Source/HktClient/Private/HktClientCoreSubsystem.cpp
+++ Plugins/HktClient/Source/HktClient/Private/HktClientCoreSubsystem.cpp
@@ -1,14 +1,50 @@
 #include "HktClientCoreSubsystem.h"
 #include "HktRpcProxy.h"
+#include "HktRpcTraits.h"
+#include "HktGraph.h"
+#include "HktDef.h"
+#include "HktPacketTypes.h"
 
 void UHktClientCoreSubsystem::Initialize(FSubsystemCollectionBase& Collection)
 {
     Super::Initialize(Collection);
     RpcProxy = MakeShared<FHktRpcProxy>(TEXT("127.0.0.1:50051"));
+    Graph = MakeShared<FHktGraph>();
 }
 
 void UHktClientCoreSubsystem::Deinitialize()
 {
     RpcProxy.Reset();
+    Graph.Reset();
     Super::Deinitialize();
+}
+
+void UHktClientCoreSubsystem::SyncGroup(int64 SubjectId, int64 GroupId)
+{
+    if (RpcProxy)
+    {
+        RpcProxy->SyncGroup(SubjectId, GroupId,
+            [this](TUniquePtr<IHktBehavior> Response)
+            {
+                if (Response->GetTypeId() == GetBehaviorTypeId<FDestroyPacket>())
+                {
+                    OnDestroyedBehavior.Broadcast(*Response);
+                    Graph->RemoveBehavior(Response->GetBehaviorId());
+                }
+                else
+                {
+                    IHktBehavior& Behavior = Graph->AddBehavior(MoveTemp(Response));
+                    OnCreatedBehavior.Broadcast(Behavior);
+                }
+            });
+    }
+}
+
+void UHktClientCoreSubsystem::ExecuteBehavior(int64 SubjectId, int32 BehaviorTypeId, const TArray<uint8>& Bytes)
+{
+    if (RpcProxy)
+    {
+		constexpr int64 GroupId = 0; // 클라이언트에서 그룹 ID를 관리하는 로직이 필요할 수 있습니다.
+        RpcProxy->ExecuteBehavior(GroupId, SubjectId, BehaviorTypeId, Bytes);
+    }
 }
--- Plugins/HktClient/Source/HktClient/Public/HktClientCoreSubsystem.h
+++ Plugins/HktClient/Source/HktClient/Public/HktClientCoreSubsystem.h
@@ -2,9 +2,15 @@
 
 #include "CoreMinimal.h"
 #include "Subsystems/GameInstanceSubsystem.h"
+#include "HktStructSerializer.h"
 #include "HktClientCoreSubsystem.generated.h"
 
+class IHktBehavior;
 class FHktRpcProxy;
+class FHktGraph;
+struct FHktPacketBase;
+
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnSyncBehavior, IHktBehavior&/* Response */);
 
 UCLASS()
 class HKTCLIENT_API UHktClientCoreSubsystem : public UGameInstanceSubsystem
@@ -15,6 +21,19 @@ public:
     virtual void Initialize(FSubsystemCollectionBase& Collection) override;
     virtual void Deinitialize() override;
 
+    void SyncGroup(int64 SubjectId, int64 GroupId);
+
+    template<typename TPacket>
+    FORCEINLINE void ExecuteBehavior(int64 SubjectId, const TPacket& Packet) 
+    { 
+        ExecuteBehavior(SubjectId, GetBehaviorTypeId<TPacket>(), FHktStructSerializer::SerializeStructToBytes(Packet));
+    }
+    void ExecuteBehavior(int64 SubjectId, int32 BehaviorTypeId, const TArray<uint8>& Bytes);
+
+    FOnSyncBehavior OnCreatedBehavior;
+    FOnSyncBehavior OnDestroyedBehavior;
+
 private:
     TSharedPtr<FHktRpcProxy> RpcProxy;
+    TSharedPtr<FHktGraph> Graph;
 };
